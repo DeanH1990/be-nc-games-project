6530d25d316877c01c9332cb5ffe07057c2d28f9
DeanH1990 <dhasley1990@gmail.com>
2022-10-10 16:49:24 +0100

    add getReviewById controller

--- controllers/app.controllers.js
+++ controllers/app.controllers.js
@@ -1,9 +1,19 @@
-const { selectCategories } = require("../models/app.models")
+const { selectCategories, selectReviewById } = require("../models/app.models")
 
 
 
 exports.getCategories = (req, res, next) => {
     selectCategories().then((categories) => {
         res.status(200).send({ categories })
+    })
+}
+
+exports.getReviewById = (req, res, next) => {
+    const { review_id } = req.params;
+    selectReviewById(review_id).then((review) => {
+        res.status(200).send({review})
+    })
+    .catch((err) => {
+        next(err)
     })
 }
