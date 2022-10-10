785ab7e724163df1a07f9ee9f96e5a9b445e7d53
DeanH1990 <dhasley1990@gmail.com>
2022-10-10 16:50:32 +0100

    add getReviewById with status 400 error handler

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require('express');
-const { getCategories } = require('./controllers/app.controllers');
+const { getCategories, getReviewById } = require('./controllers/app.controllers');
 const app = express();
 
 
 
 app.get('/api/categories', getCategories);
+app.get('/api/reviews/:review_id', getReviewById);
 
 
 app.all('*', (req, res) => {
     res.status(404).send({ msg: 'Route not found'})
+})
+
+app.use((err, req, res, next) => {
+    console.log(err.code)
+    if (err.code === '22P02') {
+        res.status(400).send({ msg: 'Invalid ID type'})
+    } else {
+        next(err)
+    }
 })
 
 
