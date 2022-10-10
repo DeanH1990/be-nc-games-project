8288a32a7d5019ab3b98a003b92f3d6fe0cafad4
DeanH1990 <dhasley1990@gmail.com>
2022-10-10 15:09:56 +0100

    remove console logs / comments

--- controllers/app.controllers.js
+++ controllers/app.controllers.js
@@ -3,7 +3,6 @@ const { selectCategories } = require("../models/app.models")
 
 
 exports.getCategories = (req, res, next) => {
-    // console.log('in the controller')
     selectCategories().then((categories) => {
         res.status(200).send({ categories })
     })
--- models/app.models.js
+++ models/app.models.js
@@ -1,7 +1,6 @@
 const db = require('../db/connection');
 
 exports.selectCategories = () => {
-    // console.log('in the model')
     return db.query('SELECT * FROM categories').then(({ rows: categories }) => {
         return categories;
     })
