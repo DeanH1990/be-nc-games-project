bd872ab2502d210e6c19f0e9a37c077e061d1f68
DeanH1990 <dhasley1990@gmail.com>
2022-10-10 15:17:21 +0100

    remove app.use(express.json()) not needed for GET

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { getCategories } = require('./controllers/app.controllers');
 const app = express();
 
-app.use(express.json())
+
 
 app.get('/api/categories', getCategories);
 
