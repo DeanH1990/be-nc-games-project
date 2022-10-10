b5f29a403b98fe4dae351846c5232b58f79b6b32
DeanH1990 <dhasley1990@gmail.com>
2022-10-10 17:16:30 +0100

    add 404 error handling for app.js

--- app.js
+++ app.js
@@ -13,11 +13,17 @@ app.all('*', (req, res) => {
 })
 
 app.use((err, req, res, next) => {
-    console.log(err.code)
     if (err.code === '22P02') {
         res.status(400).send({ msg: 'Invalid ID type'})
     } else {
         next(err)
+    }
+})
+
+app.use((err, req, res, next) => {
+    console.log(err.status)
+    if (err.status) {
+        res.status(err.status).send({ msg: err.msg })
     }
 })
 
