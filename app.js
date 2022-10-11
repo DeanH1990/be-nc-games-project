981dfae26037c345433b849d7e83501e8084ccc8
DeanH1990 <dhasley1990@gmail.com>
2022-10-11 13:47:46 +0100

    add updateReviewVotesById to app

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
-const { getCategories, getReviewById, getUsers } = require('./controllers/app.controllers');
+const { getCategories, getReviewById, getUsers, updateReviewVotesById } = require('./controllers/app.controllers');
 const app = express();
 
+app.use(express.json())
 
 
 app.get('/api/categories', getCategories);
 app.get('/api/reviews/:review_id', getReviewById);
 app.get('/api/users', getUsers);
 
+app.patch('/api/reviews/:review_id', updateReviewVotesById);
 
 app.all('*', (req, res) => {
     res.status(404).send({ msg: 'Route not found'})
@@ -22,7 +24,6 @@ app.use((err, req, res, next) => {
 })
 
 app.use((err, req, res, next) => {
-    console.log(err.status)
     if (err.status) {
         res.status(err.status).send({ msg: err.msg })
     }
