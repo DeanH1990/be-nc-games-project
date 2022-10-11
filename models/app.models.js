54d38ac660cfed91ac0a1666492adac8307c0f68
DeanH1990 <dhasley1990@gmail.com>
2022-10-11 13:49:21 +0100

    add patchReviewVotesById model

--- models/app.models.js
+++ models/app.models.js
@@ -1,7 +1,7 @@
 const db = require('../db/connection');
 
 exports.selectCategories = () => {
-    return db.query('SELECT * FROM categories').then(({ rows: categories }) => {
+    return db.query('SELECT * FROM categories;').then(({ rows: categories }) => {
         return categories;
     })
 }
@@ -9,7 +9,7 @@ exports.selectCategories = () => {
 exports.selectReviewById = (review_id) => {
     return db.query(`
     SELECT * FROM reviews
-    WHERE review_id = $1`, [review_id])
+    WHERE review_id = $1;`, [review_id])
     .then(({ rows: [review] }) => {
         if (review === undefined) {
             return Promise.reject({ status: 404, msg: 'Review ID not found'})
@@ -19,7 +19,21 @@ exports.selectReviewById = (review_id) => {
 }
 
 exports.selectUsers = () => {
-    return db.query('SELECT * FROM users').then(({ rows: users }) => {
+    return db.query('SELECT * FROM users;').then(({ rows: users }) => {
         return users
+    })
+}
+
+exports.patchReviewVotesById = (review_id, inc_votes) => {
+    if (typeof inc_votes !== 'number') {
+        return Promise.reject({ status: 400, msg: 'Please input a number'})
+    }
+    return db.query(`
+    UPDATE reviews 
+    SET votes = votes + $1
+    WHERE review_id = $2
+    RETURNING *;`, [inc_votes, review_id]
+    ).then(({ rows: [review] }) => {
+        return review
     })
 }
