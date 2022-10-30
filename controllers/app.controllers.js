ca42549a903460dbfc337174a47c23996befcf0c
DeanH1990 <dhasley1990@gmail.com>
2022-10-30 17:00:21 +0000

    update removeCommentById controller as per PR

--- controllers/app.controllers.js
+++ controllers/app.controllers.js
@@ -66,8 +66,8 @@ exports.createCommentByReviewId = (req, res, next) => {
 
 exports.removeCommentById = (req, res, next) => {
     const { comment_id } = req.params;
-    deleteCommentById(comment_id).then((body) => {
-        res.status(204).send(body)
+    deleteCommentById(comment_id).then(() => {
+        res.status(204).send()
     })
     .catch((err) => {
         next(err)
